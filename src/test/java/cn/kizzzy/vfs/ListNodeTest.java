27a31d11f1cb2e459058879a74c73756b68604e2
Kizzzy <xuhua0174@163.com>
2022-01-06 00:39:03 +0800

    misc changed

--- src/test/java/cn/kizzzy/vfs/ListNodeTest.java
+++ src/test/java/cn/kizzzy/vfs/ListNodeTest.java
@@ -5,7 +5,7 @@ import cn.kizzzy.vfs.pack.FilePackage;
 public class ListNodeTest {
     
     public static void main(String[] args) throws Exception {
-        IPackage vfs = new FilePackage("D:\\DevProject\\lib47\\lib47_java\\lib47-vfs\\src\\main\\java\\cn\\kizzzy\\vfs");
+        IPackage vfs = new FilePackage("D:\\Temp");
         //IPackage vfs = new ZipPackage("D:\\Temp.zip");
         ListNode root = vfs.list("", new ListParameter(true));
         listNode(root);
