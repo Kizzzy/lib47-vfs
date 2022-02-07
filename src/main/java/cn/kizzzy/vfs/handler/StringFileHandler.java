c92c541a026ce750a6cc6583d164f21578f49584
Kizzzy <xuhua0174@163.com>
2022-02-07 23:28:16 +0800

    [vfs] * StringFileHandler add charset constructor

--- src/main/java/cn/kizzzy/vfs/handler/StringFileHandler.java
+++ src/main/java/cn/kizzzy/vfs/handler/StringFileHandler.java
@@ -1,6 +1,17 @@
 package cn.kizzzy.vfs.handler;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class StringFileHandler extends TextFileHandler<String> {
+    
+    public StringFileHandler() {
+        super(StandardCharsets.UTF_8);
+    }
+    
+    public StringFileHandler(Charset charset) {
+        super(charset);
+    }
     
     @Override
     protected String loadImpl(String str) {
