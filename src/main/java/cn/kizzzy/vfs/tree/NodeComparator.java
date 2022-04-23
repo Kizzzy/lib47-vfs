7f691d92d21d535b50bd5fd7ce80ee653edd3e9d
Kizzzy <xuhua0174@163.com>
2022-04-24 05:18:17 +0800

    [vfs] * update NodeComparator to naturalCompare

--- src/main/java/cn/kizzzy/vfs/tree/NodeComparator.java
+++ src/main/java/cn/kizzzy/vfs/tree/NodeComparator.java
@@ -8,7 +8,7 @@ public class NodeComparator implements Comparator<Node> {
     public int compare(Node x, Node y) {
         if (x.leaf) {
             if (y.leaf) {
-                return compareName(x.name, y.name);
+                return naturalCompare(x.name, y.name);
             } else {
                 return 1;
             }
@@ -16,15 +16,43 @@ public class NodeComparator implements Comparator<Node> {
             if (y.leaf) {
                 return -1;
             } else {
-                return compareName(x.name, y.name);
+                return naturalCompare(x.name, y.name);
             }
         }
     }
     
-    private int compareName(String name1, String name2) {
-        if (name1.length() != name2.length()) {
-            return name1.length() - name2.length();
+    private int naturalCompare(String xs, String ys) {
+        if (xs == null && ys == null)
+            return 0;
+        if (xs == null)
+            return -1;
+        if (ys == null)
+            return 1;
+        
+        char[] x = xs.toCharArray();
+        char[] y = ys.toCharArray();
+        
+        int lx = x.length, ly = y.length;
+        
+        int mx = 0, my = 0;
+        for (; mx < lx && my < ly; mx++, my++) {
+            if (Character.isDigit(x[mx]) && Character.isDigit(y[my])) {
+                long vx = 0, vy = 0;
+                
+                for (; mx < lx && Character.isDigit(x[mx]); mx++)
+                    vx = vx * 10 + x[mx] - '0';
+                
+                for (; my < ly && Character.isDigit(y[my]); my++)
+                    vy = vy * 10 + y[my] - '0';
+                
+                if (vx != vy)
+                    return vx > vy ? 1 : -1;
+            }
+            
+            if (mx < lx && my < ly && x[mx] != y[my])
+                return x[mx] > y[my] ? 1 : -1;
         }
-        return name1.compareTo(name2);
+        
+        return lx - mx - (ly - my);
     }
 }
