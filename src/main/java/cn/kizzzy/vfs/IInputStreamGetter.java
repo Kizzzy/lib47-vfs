68c914c0732954116480e91f7902d65b4ec36560
Kizzzy <xuhua0174@163.com>
2025-07-02 15:06:51 +0800

    [vfs] * not print IInputStreamGetter

--- src/main/java/cn/kizzzy/vfs/IInputStreamGetter.java
+++ src/main/java/cn/kizzzy/vfs/IInputStreamGetter.java
@@ -1,7 +1,9 @@
 package cn.kizzzy.vfs;
 
+import cn.kizzzy.format.Ignore;
 import cn.kizzzy.io.IFullyReader;
 
+@Ignore
 public interface IInputStreamGetter {
     
     IFullyReader getInput() throws Exception;
